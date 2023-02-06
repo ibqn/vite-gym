c929f803c3247f7079810c07f3ab4d183022ac84
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-02-06 17:57:56 +0100

    add animations for home page

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -32,20 +32,41 @@ export const Home = (props: Props) => {
     >
       <div className="grid grid-cols-1 grid-rows-2 pt-32 md:grid-cols-2 md:grid-rows-1">
         <div className="flex flex-col justify-center">
-          <div className="relative flex">
-            <div className="absolute -top-16 -left-16 z-0 flex">
-              <img src={EvolveText} alt="Evolve Text" />
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <div className="relative flex">
+              <div className="absolute -top-16 -left-16 z-0 flex">
+                <img src={EvolveText} alt="Evolve Text" />
+              </div>
+              <img className="z-10" src={HomePageText} alt="Home Page Text" />
             </div>
-            <img className="z-10" src={HomePageText} alt="Home Page Text" />
-          </div>
 
-          <p className="mt-8 text-sm">
-            Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
-            Studios to get the Body Shapes That you Dream of... Get Your Dream
-            Body Now.
-          </p>
+            <p className="mt-8 text-sm">
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of... Get Your Dream
+              Body Now.
+            </p>
+          </motion.div>
 
-          <div className="mt-8 flex items-center gap-8">
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <ActionButton link={SelectedPage.CONTACT_US}>Join Now</ActionButton>
             <AnchorLink
               className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
@@ -53,12 +74,22 @@ export const Home = (props: Props) => {
             >
               learn more
             </AnchorLink>
-          </div>
+          </motion.div>
         </div>
 
-        <div className="flex flex-1  justify-center md:justify-items-end">
+        <motion.div
+          className="flex flex-1 justify-center md:justify-items-end"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: 50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
           <img src={HomePageGraphic} alt="Home Page Graphic" />
-        </div>
+        </motion.div>
       </div>
 
       <div className="hidden bg-primary-100 py-8 md:flex">
