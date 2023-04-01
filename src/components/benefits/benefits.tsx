743b353157ab1af8fcc202d431a56dcee411e514
ibqn <evgen.ibqn@gmail.com>
2023-04-01 20:00:24 +0200

    complete benefits

--- src/components/benefits/benefits.tsx
+++ src/components/benefits/benefits.tsx
@@ -5,6 +5,7 @@ import { benefits } from './items'
 import { BenefitItem } from '@/components/benefit-item'
 import { HTitle } from '@/components/h-title'
 import BenefitsPageGraphic from '@/assets/benefits-page-graphic.png'
+import { ActionButton } from '../action-button'
 
 type Props = {}
 
@@ -48,12 +49,67 @@ export const Benefits = (props: Props) => {
         })}
       </motion.div>
 
-      <div className="mt-16 flex items-center justify-between gap-20 md:mt-28">
+      <div className="mt-16 flex flex-col items-center justify-between gap-20 md:mt-28 md:flex-row">
         <img
           className="mx-auto"
           src={BenefitsPageGraphic}
           alt="Benefits page graphic"
         />
+
+        <div>
+          <div className="relative">
+            <div className="before:absolute before:-left-20 before:-top-20 before:z-[1] before:content-abstractwaves">
+              <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ duration: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, x: 50 },
+                  visible: { opacity: 1, x: 0 },
+                }}
+              >
+                <HTitle>
+                  millions of happy members getting{' '}
+                  <span className="text-primary-500">fit</span>
+                </HTitle>
+              </motion.div>
+            </div>
+          </div>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: 50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <p className="my-5">
+              Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
+              egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
+              fames vitae vitae quis. Quis amet vulputate tincidunt at in nulla
+              nec. Consequat sed facilisis dui sit egestas ultrices tellus.
+              Ullamcorper arcu id pretium sapien proin integer nisl. Felis orci
+              diam odio.
+            </p>
+            <p className="mb-5">
+              Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+              tellus quam porttitor. Mauris velit euismod elementum arcu neque
+              facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+              enim mattis odio in risus nunc.
+            </p>
+          </motion.div>
+
+          <div className="relative mt-16">
+            <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+              <ActionButton link={SelectedPage.CONTACT_US}>
+                Join Now
+              </ActionButton>
+            </div>
+          </div>
+        </div>
       </div>
     </Section>
   )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,17 +17,17 @@ module.exports = {
       backgroundImage: (theme) => ({
         'gradient-yellowred':
           'linear-gradient(90deg, #FF616A 0%, #FFC837 100%)',
-        'mobile-home': "url('./assets/HomePageGraphic.png')",
+        'mobile-home': "url('./assets/home-page-graphic.png')",
       }),
       fontFamily: {
         dmsans: ['DM Sans', 'sans-serif'],
         montserrat: ['Montserrat', 'sans-serif'],
       },
       content: {
-        evolvetext: "url('./assets/EvolveText.png')",
-        abstractwaves: "url('./assets/AbstractWaves.png')",
-        sparkles: "url('./assets/Sparkles.png')",
-        circles: "url('./assets/Circles.png')",
+        evolvetext: "url('./assets/evolve-text.png')",
+        abstractwaves: "url('./assets/abstract-waves.png')",
+        sparkles: "url('./assets/sparkles.png')",
+        circles: "url('./assets/circles.png')",
       },
     },
     screens: {
