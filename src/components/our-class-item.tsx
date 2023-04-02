bcce2e1ef1b84ac36cd783855019d1ca75b7c5da
ibqn <evgen.ibqn@gmail.com>
2023-04-02 23:38:28 +0200

    complete our classes

--- src/components/our-class-item.tsx
+++ src/components/our-class-item.tsx
@@ -1,13 +1,25 @@
 import { type ComponentProps } from 'react'
 import { type OurClass } from '@/components/our-classes/items'
+import { classNames } from '@/util/class-names'
 
 type Props = { ourClass: OurClass } & ComponentProps<'li'>
 
-export const OurClassItem = (props: Props) => {
-  const { image } = props.ourClass
+export const OurClassItem = ({ ourClass }: Props) => {
+  const { image, name, description } = ourClass
 
   return (
-    <li className="h-auto w-[450px]">
+    <li className="relative h-auto w-[450px]">
+      <div
+        className={classNames(
+          'absolute z-10 flex h-full w-full',
+          'flex-col items-center justify-center whitespace-normal',
+          'bg-primary-500 p-5 text-center text-white',
+          'opacity-0 transition duration-500 hover:opacity-90'
+        )}
+      >
+        <p className="text-2xl">{name}</p>
+        <p className="mt-4">{description}</p>
+      </div>
       <img src={image} alt="Our classes" />
     </li>
   )
