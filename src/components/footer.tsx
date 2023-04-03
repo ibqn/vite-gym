34f5e7a16b3e00e20f9a0b285d09244b203ea9f7
ibqn <evgen.ibqn@gmail.com>
2023-04-03 09:40:04 +0200

    complete footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = (props: Props) => {
   return (
     <footer className="bg-primary-100 py-16">
       <div className="container mx-auto flex gap-16">
-        <div className="mt-16 basis-1/2 md:mt-0">
+        <div className="basis-1/2">
           <img src={Logo} alt="Logo" />
           <p className="my-5">
             Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
@@ -15,8 +15,31 @@ export const Footer = (props: Props) => {
           </p>
           <p>© Evogym All Rights Reserved.</p>
         </div>
-        <div className="basis-1/4 bg-yellow-200"></div>
-        <div className="basis-1/4 bg-yellow-300"></div>
+        <ul className="flex basis-1/4 flex-col gap-y-5">
+          <li>
+            <h4 className="font-bold">Links</h4>
+          </li>
+          <li>
+            <p>Massa orci senectus</p>
+          </li>
+          <li>
+            <p>Et gravida id et etiam</p>
+          </li>
+          <li>
+            <p>Ullamcorper vivamus</p>
+          </li>
+        </ul>
+        <ul className="flex basis-1/4 flex-col gap-y-5">
+          <li>
+            <h4 className="font-bold">Contact Us</h4>
+          </li>
+          <li>
+            <p>Massa orci senectusTempus metus mattis risus volutpat egestas</p>
+          </li>
+          <li>
+            <p>Tel.: (333)425-6825</p>
+          </li>
+        </ul>
       </div>
     </footer>
   )
